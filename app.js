218fcc1fafdf27ba27881810b281472aebfe90ec
JPeralta <josue@picnic.pe>
2019-03-21 14:16:04 -0500

    json

---
+++ app.js
@@ -0,0 +1,25 @@
+// 
+
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+app.use(express.static('public'));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  
+  
+    socket.on('canal', function(msg){
+      console.log(msg);
+      io.emit('canal',msg);
+      });
+});
+
+http.listen(3001, function(){
+  console.log('listening on *:3001');
+});
