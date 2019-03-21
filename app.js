2dec2e75be77ec9cc57f8e505656292ffe9586f1
JPeralta <josue@picnic.pe>
2019-03-21 14:25:47 -0500

    wea

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3001, function(){
-  console.log('listening on *:3001 '+__dirname);
+  console.log('listening on *:3001 wea '+__dirname);
 });
