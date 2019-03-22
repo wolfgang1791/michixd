520756313448b9e5e31912961e6a1081af64088c
JPeralta <josue@picnic.pe>
2019-03-22 18:42:34 -0500

    fuck+

--- app.js
+++ app.js
@@ -17,24 +17,62 @@ app.set('view engine', 'jade');
 // console.log(myURL.hostname);
 var canal = '';
 
+var jugadores = [];
+
 app.get('/', function(req, res){
   res.render( __dirname + '/index'); // base_url fake para heroku
 });
 
-app.get(`/tablero`, function(req, res){
+app.get('/armas',(req,res)=>{
   
   canal = req.query.sala;
   usuario = req.query.usuario;
- 
-  console.log(req);
+  // arma = req.query.arma;
+  var userid = id(100);
+  
+  // console.log(req);
   
 
   console.log(usuario);
   
   console.log(canal);
+
   
-  res.render(__dirname + '/tablero',{sala:canal});
-  // res.sendFile(__dirname + '/tablero',{sala:canal}); 
+
+  if(usuario != null){
+    jugadores.push({ 'id': userid,'user':usuario});
+  }
+
+  
+
+  if(jugadores[0].arma != null){
+      if(jugadores[0].arma == 'x'){
+        jugadores[1].arma = 'o';
+      }
+      else if(jugadores[0].arma == 'o'){
+        jugadores[1].arma = 'x';
+      }
+      console.log(jugadores);
+      res.render(__dirname + '/tablero',{sala:canal,jugador1:jugadores[0].user,jugador2:jugadores[1].user,usuario:usuario});
+  }
+  else{
+    console.log(jugadores);
+    res.render(__dirname + '/armas',{id:userid});
+  }
+});
+
+app.get(`/tablero`, function(req, res){
+  
+   arma = req.query.arma;
+
+   jugadores[0].arma = arma;
+
+   console.log(jugadores);
+  
+    console.log('usaurio '+usuario);
+    
+   res.render(__dirname + '/tablero',{sala:canal,jugador1:jugadores[0].user,jugador2:jugadores[1].user,usuario:usuario});
+
 });
 
 
@@ -43,11 +81,41 @@ io.on('connection', function(socket){
     console.log(canal);
     
     socket.on(canal, function(usuario,msg){
-      console.log(msg+" "+usuario);
-      io.emit(canal,msg);
+
+      
+
+
+      console.log(usuario+" "+msg);
+        io.emit(canal,msg,jugadores,usuario);
+    
       });
 });
 
 http.listen(port, function(){
   console.log('listening on *:3001 ');
 });
+
+var id = (par) =>{
+  return Math.round(Math.random()*par);
+}
+
+// var turno_ = (jugadores,user) => {
+//   var temp = '';
+//   var index = -1;
+//   for (let i = 0; i < jugadores.length; i++) {
+       
+//        if(user == jugadores[i].user){
+//            index = i;
+//        }
+//       c("i "+i);
+//       }  
+          
+//       if( index == 0){
+//           temp = jugadores[1].user;
+//       }
+//       else if( index == 1){
+//           temp = jugadores[0].user;
+//       }
+              
+//       return temp;
+// }
